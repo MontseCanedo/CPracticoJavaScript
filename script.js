087d7f24c1459e2884e266df0036c4dfafbfe526
Montserrat Canedo Gutiérrez <vmontse_cgutierrez@hotmail.com>
2023-03-15 16:56:57 -0400

    addEventListener

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
-console.log(input.value);
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
+form.addEventListener('submit', btnOnclick);
+
+function btnOnclick(event) {
+    console.log(event);
+    event.preventDefault();
+    const sum = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sum;
+}
